ce23d154985bec55c315c4c7481c704ee900caa7
Manshikumari <88151072+Manshikumari@users.noreply.github.com>
2023-10-30 12:29:48 +0000

    pedning

--- clock.js
+++ clock.js
@@ -1,8 +1,31 @@
-var result = document.getElementById('#start')
-var sec = sec+1
+
+
+var hr = 0;
+var min =0;
+var sec = 0;
+
+var time=false;
+
 function start()
 {
-    result.innerHTML="start"
-    sec++
-    setInterval (start,10)
+    time=true;
+    digiwatch();
+}
+function stop()
+{
+    time = false;
+}
+function Reset()
+{
+    time = false;
+}
+function digiwatch()
+{
+  if(time == true)
+  { 
+    sec = sec +1;
+    document.getElementById("sec").innerHTML = sec;
+   setTimeout ( "digiwatch()" , 10)
+  }
+
 }
