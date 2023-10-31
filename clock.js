e80f0a2a03390f7c704f185f490e59b025d02df4
Manshikumari <88151072+Manshikumari@users.noreply.github.com>
2023-10-31 10:02:13 +0000

    pending1

--- clock.js
+++ clock.js
@@ -3,7 +3,7 @@
 var hr = 0;
 var min =0;
 var sec = 0;
-
+var count = 0;
 var time=false;
 
 function start()
@@ -23,9 +23,30 @@ function digiwatch()
 {
   if(time == true)
   { 
-    sec = sec +1;
-    document.getElementById("sec").innerHTML = sec;
+    count = count+1;
+     if(count == 100)
+     {
+       sec = sec + 1;
+       count = 0;
+
+     }
+    
+     if(sec == 60)
+     {
+        mint = mint+1;
+        sec  = 0 ;
+
+     }
+     if(mint == 60 )
+     {
+       hr = hr+1 ;
+       mint = 0;
+       sec = 0;
+     }
+     document.getElementById("hr").innerHTML = hr;
+     document.getElementById("mint").innerHTML = mint;
+     document.getElementById("sec").innerHTML = sec;
+    document.getElementById("count").innerHTML = count;
    setTimeout ( "digiwatch()" , 10)
   }
-
 }
